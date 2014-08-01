00b2e07db1da6637f0c5d4a90ba0c8489862c15e
stelter <stelter@tzi.de>
2014-08-01 17:15:57 +0200

    fixed build

--- suturo_manipulation_moveit/src/arm_controller.cpp
+++ suturo_manipulation_moveit/src/arm_controller.cpp
@@ -13,47 +13,13 @@ using namespace std;
 typedef actionlib::ActionServer<control_msgs::FollowJointTrajectoryAction> Server;
 ros::ServiceClient move_along_joint_path_client;
 
-// void follow_joint_trajectory(const control_msgs::FollowJointTrajectoryGoalConstPtr &goal, ros::NodeHandle *nh, Server *server_arm)
-// {
-//     // if (server_arm->)
-//     euroc_c2_msgs::MoveAlongJointPath move_along_joint_path_srv;
-//     move_along_joint_path_srv.request.joint_names = goal->trajectory.joint_names;
-
-//     const unsigned int nr_lwr_joints = 7;
-
-//     // move_along_joint_path_srv.request.joint_names = lwr_joints; // Select all lwr joints
-
-//     for (std::vector<trajectory_msgs::JointTrajectoryPoint>::const_iterator p = goal->trajectory.points.begin(); p != goal->trajectory.points.end(); ++p)
-//     {
-//         euroc_c2_msgs::Configuration configuration;
-//         configuration.q = p->positions;
-//         move_along_joint_path_srv.request.path.push_back(configuration);
-//     }
-
-
-//     move_along_joint_path_srv.request.joint_limits.resize(nr_lwr_joints);
-//     for (unsigned int i = 0; i < nr_lwr_joints; ++i)
-//     {
-//         euroc_c2_msgs::Limits &limits = move_along_joint_path_srv.request.joint_limits[i];
-//         limits.max_velocity = 20 * M_PI / 180.0; // 20 degrees per second
-//         limits.max_acceleration = 400 * M_PI / 180.0;
-//     }
-
-//     move_along_joint_path_client.call(move_along_joint_path_srv);
-//     std::string &move_error_message = move_along_joint_path_srv.response.error_message;
-//     if (!move_error_message.empty())
-//     {
-//         std::cout << "Move failed: " + move_error_message << std::endl;
-//         server_arm->setAborted();
-//     }
-//     ROS_INFO_STREAM("adsdff");
-//     server_arm->setSucceeded();
-// }
-
 void goal_call_back(Server::GoalHandle gh)
 {
-    ROS_INFO_STREAM("goal");
-    control_msgs::FollowJointTrajectoryGoalConstPtr goal = gh.getGoal();
+    Server::GoalHandle current_gh_ = gh;
+    current_gh_.setAccepted();
+  
+
+    control_msgs::FollowJointTrajectoryGoal goal = *gh.getGoal();
     euroc_c2_msgs::MoveAlongJointPath move_along_joint_path_srv;
     move_along_joint_path_srv.request.joint_names = goal.trajectory.joint_names;
 
@@ -61,7 +27,7 @@ void goal_call_back(Server::GoalHandle gh)
 
     // move_along_joint_path_srv.request.joint_names = lwr_joints; // Select all lwr joints
 
-    for (std::vector<trajectory_msgs::JointTrajectoryPoint>::const_iterator p = goal->trajectory.points.begin(); p != goal->trajectory.points.end(); ++p)
+    for (std::vector<trajectory_msgs::JointTrajectoryPoint>::const_iterator p = goal.trajectory.points.begin(); p != goal.trajectory.points.end(); ++p)
     {
         euroc_c2_msgs::Configuration configuration;
         configuration.q = p->positions;
@@ -82,15 +48,14 @@ void goal_call_back(Server::GoalHandle gh)
     if (!move_error_message.empty())
     {
         std::cout << "Move failed: " + move_error_message << std::endl;
-        gh->setAborted();
+        gh.setAborted();
     }
-    ROS_INFO_STREAM("adsdff");
-    gh->setSucceeded();
+    gh.setSucceeded();
 }
 
 void cancel_call_back(Server::GoalHandle gh)
 {
-    ROS_INFO_STREAM("cancel");
+    ROS_ERROR_STREAM("cancel");
 }
 
 int main(int argc, char **argv)
@@ -105,12 +70,15 @@ int main(int argc, char **argv)
     move_along_joint_path_client = n.serviceClient<euroc_c2_msgs::MoveAlongJointPath>(move_along_joint_path);
 
     // Server server_arm(n, "/arm_controller/follow_joint_trajectory", boost::bind(&follow_joint_trajectory, _1, &n, &server_arm), false);
-    Server arm_server(n, "/arm_controller/follow_joint_trajectory",
-        boost::bind(&goal_call_back, this, _1),
-        boost::bind(&cancel_call_back, this, _1),
-        false);
+    Server arm_server(n, "/arm_controller/follow_joint_trajectory", false   );
 
-    server_arm.start();
+    arm_server.registerGoalCallback(&cancel_call_back);
+    arm_server.registerGoalCallback(&goal_call_back);
+        // boost::bind(&goal_call_back, _1),
+        // boost::bind(&cancel_call_back, _1),
+        // false);
+
+    arm_server.start();
 
     ROS_INFO("Ready to move the arms!.");
     ros::spin();
